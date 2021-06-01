863e3f7ff7954407cdcc8ed444e6045397f2f58d
ZloiSlonik <olga.larch@yandex.ru>
2021-06-01 22:15:54 +0300

    Update MyFirstProgram.java

--- Sandbox/src/main/java/ru/swt/my/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/swt/my/MyFirstProgram.java
@@ -3,6 +3,6 @@ package ru.swt.my;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-      System.out.println("Hello, world!");
-  }
+        System.out.println("Hello, world!");
+    }
 }
