a4095a5ca09bd449b3d096c6cef69b9a27f566cb
ZloiSlonik <olga.larch@yandex.ru>
2021-06-02 21:04:44 +0300

    Добавлено несколько функций из лекции 1.7

--- Sandbox/src/main/java/ru/swt/my/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/swt/my/MyFirstProgram.java
@@ -3,6 +3,31 @@ package ru.swt.my;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+       hello("Lara");
+        hello("Olga");
+        hello("Bubu");
+        double len = 6;
+        double a = 8;
+        double b = 7;
+        System.out.println("Площадь квардарат со стороной " + len + "=" +  area(len));
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area (a,b));
+
     }
+
+    public static int myFunction(int a, int b) {
+        return a+b;
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area (double l) {
+        return l*l;
+    }
+
+    public static double area (double a, double b) {
+        return a*b;
+    }
+
 }
