550fa9d0cad93faaf38bbcd03cf307acb4c9a792
ZloiSlonik <olga.larch@yandex.ru>
2021-06-16 00:08:09 +0300

    Создан базовый класс HelperBase, добавлены методы click и type.  Оптимизированы Contact, Group, Session, Navi Helper

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/appmanager/ContactHelper.java
@@ -5,48 +5,32 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import ru.swt.addressbook.model.PersonData;
 
-public class ContactHelper {
+public class ContactHelper extends HelperBase{
 
-    private FirefoxDriver wd;
-
-    public ContactHelper(FirefoxDriver wd) {
-        this.wd = wd;
+      public ContactHelper(FirefoxDriver wd) {
+        super(wd);
     }
 
     public void submitPersonCreation() {
-        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+        click(By.xpath("(//input[@name='submit'])[2]"));
     }
 
     public void fillPersonForm(PersonData personData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(personData.getFirst_name());
-        wd.findElement(By.name("theform")).click();
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(personData.getLast_name());
-        wd.findElement(By.xpath("//div[@id='content']/form/label[4]")).click();
-        wd.findElement(By.name("nickname")).click();
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys(personData.getNick_name());
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(personData.getCompany_name());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).sendKeys(personData.getEmail());
-        wd.findElement(By.name("bday")).click();
+        type (By.name("firstname"), personData.getFirst_name());
+        type (By.name("lastname"), personData.getLast_name());
+        type (By.name("nickname"), personData.getNick_name());
+        type (By.name("company"), personData.getCompany_name());
+        type (By.name("email"), personData.getEmail());
         new Select(wd.findElement(By.name("bday"))).selectByVisibleText(personData.getBd_date());
-        wd.findElement(By.xpath("//option[@value='" + personData.getBd_date() + "']")).click();
-        wd.findElement(By.name("bmonth")).click();
+        click(By.xpath("//option[@value='" + personData.getBd_date() + "']"));
+        click(By.name("bmonth"));
         new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(personData.getBd_month());
-        wd.findElement(By.xpath("//option[@value='" + personData.getBd_month() + "']")).click();
-        wd.findElement(By.name("byear")).click();
-        wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys(personData.getBd_year());
-        wd.findElement(By.name("theform")).click();
+        click(By.xpath("//option[@value='" + personData.getBd_month() + "']"));
+        type (By.name("byear"), personData.getBd_year());
+        click (By.name("theform"));
     }
 
     public void initPersonCreation() {
-        wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 }
