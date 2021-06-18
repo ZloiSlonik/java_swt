f459ec8b6ae37e745f20a2c9799d713abb461c87
ZloiSlonik <olga.larch@yandex.ru>
2021-06-18 19:56:40 +0300

    Добавлена возможность запуска из разных браузеров (Chrome, FF, MS Edge)

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/appmanager/ApplicationManager.java
@@ -1,20 +1,43 @@
 package ru.swt.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-    private FirefoxDriver wd;
+    WebDriver wd;
 
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
+
+        if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        }
+
+        else if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        }
+
+        else if (browser == BrowserType.EDGE) {
+            wd = new EdgeDriver();
+
+        }
         //    driver = new ChromeDriver();
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
--- addressbook-web-tests/src/test/java/ru/swt/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/appmanager/ContactHelper.java
@@ -1,13 +1,14 @@
 package ru.swt.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import ru.swt.addressbook.model.PersonData;
 
 public class ContactHelper extends HelperBase {
 
-    public ContactHelper(FirefoxDriver wd) {
+    public ContactHelper(WebDriver wd) {
         super(wd);
     }
 
