76efd9e0f3830d91e0525809db2284160b943540
ZloiSlonik <olga.larch@yandex.ru>
2021-06-21 23:33:51 +0300

    GroupDeletionTest: добавлено создание группы если их нет

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupDeletionTest.java
@@ -1,12 +1,16 @@
 package ru.swt.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.swt.addressbook.model.GroupData;
 
 public class GroupDeletionTest extends TestBase {
 
     @Test
     public void testGroupDeletion() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+         app.getGroupHelper().createGroup(new GroupData("Group_added", "Test88", null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroup();
         app.getGroupHelper().returnToGroupPage();
