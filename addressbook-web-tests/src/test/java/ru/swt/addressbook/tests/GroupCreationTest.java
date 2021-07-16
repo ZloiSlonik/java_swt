302f2350b7ceb85950cd7e0290a0f1c5d0b4e115
ZloiSlonik <olga.larch@yandex.ru>
2021-07-16 20:38:04 +0300

    Оптимизирован GroupCreationTest - добавлена анонимная функция вычисления максимального идентификатора

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.swt.addressbook.model.GroupData;
 
+
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,18 +14,12 @@ public class GroupCreationTest extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("zzz", "yyy", "xxx");
+        GroupData group = new GroupData("ZooZ", "yyy", "xxx");
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() +1);
 
-              int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 
