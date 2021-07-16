95dfc9e6bd6da1432bf93f1152527bd70c22bf3d
ZloiSlonik <olga.larch@yandex.ru>
2021-07-16 20:21:47 +0300

    Добавлено сравнение списков в тест GroupCreation

--- addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/swt/addressbook/tests/GroupCreationTest.java
@@ -3,6 +3,8 @@ package ru.swt.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.swt.addressbook.model.GroupData;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase {
@@ -11,10 +13,22 @@ public class GroupCreationTest extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("zzz", "yyy", "xxx"));
+        GroupData group = new GroupData("zzz", "yyy", "xxx");
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() +1);
-        app.logout();
+
+              int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+
+
     }
 
 }
